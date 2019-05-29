b834745d9a4fcca388e7125d4767ee6f50cec9bf
Jacob Buitelaar <j.buitelaar@robeco.nl>
2019-05-29 23:51:44 +0200

    refactor a bit

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ The main challenge is that you need to refresh the cookie every week, as describ
 # Usage
 
 1. First you need to get the cookies. I can no longer get it to work in Chrome, but this works in Firefox:
-   1. go to https://www.strava.com/heatmap and login. Zoom in on the map to make sure the cookie gets stored.
+   1. go to https://www.strava.com/heatmap and login. 
+   1. Zoom in on the map to make sure the cookie gets stored.
 1. run the script `strava.py`. It creates an xml file.
 1. copy to file to your phone in Locus/mapsOnline/custom
 1. start locus, and it will find the new maps.
--- strava.py
+++ strava.py
@@ -1,38 +1,63 @@
 import browser_cookie3
 from datetime import datetime
+from pathlib import Path
 
 # parameters:
-id = 99901  # needs to be unique across Locus maps
-filename = "strava"
-suffix='jb'
+provider_id = 99901  # needs to be unique across Locus maps
+filename = Path().home() / "Downloads" / "strava.xml"
+suffix = ' jb'  # add to the name (if anything)
 
-cj = browser_cookie3.firefox(domain_name='strava.com')
-cj.clear_expired_cookies()  # not sure this does anything
-
-cookie_names = [
-    "CloudFront-Key-Pair-Id",
-    "CloudFront-Policy",
-    "CloudFront-Signature"
+# for every map: (Name to display, url field, color)
+provider_fields = [
+    ( "Biking", "ride", "bluered" ),
+    ( "Running", "run", "bluered" )
 ]
 
-cookie_elements = []
-max_expiry = 0
-for cookie in cj:
-    if cookie.name in cookie_names:
-        cookie_elements.append(f"{cookie.name}={cookie.value};")
-        max_expiry = max(max_expiry, cookie.expires)
 
-expiry_dt = datetime.fromtimestamp(max_expiry)
+def get_cookie():
+    cj = browser_cookie3.firefox(domain_name='strava.com')
+    cj.clear_expired_cookies()  # not sure this does anything
 
-assert len(cookie_elements) == len(cookie_names), "not all cookie elements found"
-assert expiry_dt > datetime.now(), f"Cookies expired on {expiry_dt}"
+    cookie_names = [
+        "CloudFront-Key-Pair-Id",
+        "CloudFront-Policy",
+        "CloudFront-Signature"
+    ]
 
-cookie_txt = "".join(cookie_elements)
-#print(f'Cookie#{cookie_txt}')
+    cookie_elements = []
+    max_expiry = 0
+    for cookie in cj:
+        if cookie.name in cookie_names:
+            cookie_elements.append(f"{cookie.name}={cookie.value};")
+            max_expiry = max(max_expiry, cookie.expires)
 
-# todo: generalize template for other colours and sports. Can create multiple providers as they are all the same
-template = open('strava_template.xml').read()
-content = template.format(id1=id, id2=id + 1, cookie=cookie_txt, suffix=suffix)
+    expiry_dt = datetime.fromtimestamp(max_expiry)
 
-with open(f'{filename}.xml', 'w+') as f:
-    f.write(content)
+    assert len(cookie_elements) == len(cookie_names), "not all cookie elements found"
+    assert expiry_dt > datetime.now(), f"Cookies expired on {expiry_dt}"
+
+    cookie_txt = "".join(cookie_elements)
+    # print(f'Cookie#{cookie_txt}')
+    return cookie_txt
+
+
+content_format = """<?xml version="1.0" encoding="UTF-8"?>
+<providers>
+
+{}
+
+</providers>"""
+
+if __name__ == "__main__":
+    cookie_txt = get_cookie()
+    template = open(str('strava_template.xml')).read()
+
+    providers = [
+        template.format(id=provider_id + idx, name=name, type=activity, cookie=cookie_txt, suffix=suffix, color=color)
+        for idx, (name, activity, color) in enumerate(provider_fields)
+    ]
+
+    content = content_format.format("\n\n".join(providers))
+
+    with open(filename, 'w+') as f:
+        f.write(content)
