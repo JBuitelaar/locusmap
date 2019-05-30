faba5c8c3660a6f7a2cc49ccd54cfc88745faeb3
Jacob Buitelaar <j.buitelaar@robeco.nl>
2019-05-30 15:15:26 +0200

    use an easier method to get the cookies

--- README.md
+++ README.md
@@ -3,14 +3,16 @@ Script to create the XML file to use [Strava heatmaps](https://www.strava.com/he
 The main challenge is that you need to refresh the cookie every week, as described [here](https://help.locusmap.eu/topic/strava-heatmap-requires-now-authentification-at-higher-zoom#comment-70872).
 
 # Usage
-
-1. First you need to get the cookies. I can no longer get it to work in Chrome, but this works in Firefox:
-   1. go to https://www.strava.com/heatmap and login. 
-   1. Zoom in on the map to make sure the cookie gets stored.
+1. enter your login details in strava_login.json
 1. run the script `strava.py`. It creates an xml file.
 1. copy to file to your phone in Locus/mapsOnline/custom
 1. start locus, and it will find the new maps.
 
+It uses a trick described [here](https://github.com/nnngrach/strava_auto_auth). Alternatively, you can
+get the cookies from a browser. For that you need to go to https://www.strava.com/heatmap and login. 
+Next zoom in on the map to make sure the cookie gets stored. I can no longer get it to work in Chrome, but this 
+works in Firefox. After that, you can use the method `get_cookies_from_browser` to create the cookie string.
+See the commented out code.
 
 
 
--- strava.py
+++ strava.py
@@ -1,6 +1,9 @@
 import browser_cookie3
 from datetime import datetime
 from pathlib import Path
+import requests
+import json
+from collections import namedtuple
 
 # parameters:
 provider_id = 99901  # needs to be unique across Locus maps
@@ -9,35 +12,51 @@ suffix = ' jb'  # add to the name (if anything)
 
 # for every map: (Name to display, url field, color)
 provider_fields = [
-    ( "Biking", "ride", "bluered" ),
-    ( "Running", "run", "bluered" )
+    ("Biking", "ride", "bluered"),
+    ("Running", "run", "bluered")
+]
+
+cookie_names = [
+    "CloudFront-Key-Pair-Id",
+    "CloudFront-Policy",
+    "CloudFront-Signature"
 ]
 
 
-def get_cookie():
+def get_cookies_from_browser():
+    """read the relevant cookies from the browser after you've logged in to Strava via FireFox"""
     cj = browser_cookie3.firefox(domain_name='strava.com')
     cj.clear_expired_cookies()  # not sure this does anything
 
-    cookie_names = [
-        "CloudFront-Key-Pair-Id",
-        "CloudFront-Policy",
-        "CloudFront-Signature"
-    ]
+    cookies = [c for c in cj if c.name in cookie_names]
+    return cookies
 
-    cookie_elements = []
-    max_expiry = 0
-    for cookie in cj:
-        if cookie.name in cookie_names:
-            cookie_elements.append(f"{cookie.name}={cookie.value};")
-            max_expiry = max(max_expiry, cookie.expires)
 
-    expiry_dt = datetime.fromtimestamp(max_expiry)
+def get_cookies_from_api():
+    # see: https://github.com/nnngrach/strava_auto_auth
+    auth_url = 'https://api.apify.com/v2/acts/nnngrach~strava-auth/run-sync?token=ATnnxbF6sE7zEZDmMbZTTppKo'
+    cookie_url = 'https://api.apify.com/v2/acts/nnngrach~strava-auth/runs/last/dataset/items?token=ATnnxbF6sE7zEZDmMbZTTppKo'
 
-    assert len(cookie_elements) == len(cookie_names), "not all cookie elements found"
+    with open('strava_login.json') as f:
+        data = json.load(f)
+    assert data['password'] != "*", "update password in strava_login.json"
+    res = requests.post(auth_url, json=data)
+    assert 201 == res.status_code
+    cookies_list = requests.get(cookie_url).json()
+
+    # make it look like a cookie object:
+    cookie = namedtuple('cookie', ('name', 'value', 'expires'))
+    cookies = [cookie(c['name'], c['value'], c['expires']) for c in cookies_list if c['name'] in cookie_names]
+    return cookies
+
+
+def get_cookie(cookies):
+    assert len(cookies) == 3
+    expiry_dt = datetime.fromtimestamp(min(c.expires for c in cookies))
     assert expiry_dt > datetime.now(), f"Cookies expired on {expiry_dt}"
-
-    cookie_txt = "".join(cookie_elements)
-    # print(f'Cookie#{cookie_txt}')
+    print("Cookies will expire on ", expiry_dt)
+    assert len(cookies) == len(cookie_names), "not all cookie elements found"
+    cookie_txt = "; ".join(f"{c.name}={c.value}" for c in cookies)
     return cookie_txt
 
 
@@ -49,8 +68,12 @@ content_format = """<?xml version="1.0" encoding="UTF-8"?>
 </providers>"""
 
 if __name__ == "__main__":
-    cookie_txt = get_cookie()
-    template = open(str('strava_template.xml')).read()
+    cookies = get_cookies_from_api()
+    # alternatively, you could use this, but you'd first need to login to strava using Firefox
+    # cookies = get_cookies_from_browser()
+    cookie_txt = get_cookie(cookies)
+
+    template = open('strava_template.xml').read()
 
     providers = [
         template.format(id=provider_id + idx, name=name, type=activity, cookie=cookie_txt, suffix=suffix, color=color)
